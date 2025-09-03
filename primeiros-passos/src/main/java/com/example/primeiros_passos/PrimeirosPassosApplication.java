6e005a6f416cb3ac9c4754ec71b16b8e32363095
Diogo Montalvão <109600744+DiogoMontalvao@users.noreply.github.com>
2025-09-03 09:55:17 -0300

    2.2.2.6 Configuration Properties

--- primeiros-passos/src/main/java/com/example/primeiros_passos/PrimeirosPassosApplication.java
+++ primeiros-passos/src/main/java/com/example/primeiros_passos/PrimeirosPassosApplication.java
@@ -1,10 +1,7 @@
 package com.example.primeiros_passos;
 
-import com.example.primeiros_passos.app.SistemaMensagem;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
--- primeiros-passos/src/main/java/com/example/primeiros_passos/app/SistemaMensagem.java
+++ primeiros-passos/src/main/java/com/example/primeiros_passos/app/SistemaMensagem.java
@@ -1,27 +1,20 @@
 package com.example.primeiros_passos.app;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
 
-    @Value("${name:valorAlternativo}")
-    private String nome;
-
-    @Value("${email}")
-    private String email;
-
-    @Value("${telefone}")
-    private List<Long> telefone;
+    @Autowired
+    Remetente remetente;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.printf(
-                "Mensagem enviada por: %s\nEmail: %s\nTelefones para contato: %s\n", nome, email, telefone
+                "Mensagem enviada por: %s\nEmail: %s\nTelefones para contato: %s\n",
+                remetente.getNome(), remetente.getEmail(), remetente.getTelefones()
         );
         System.out.println("Seu cadastro foi aprovado!");
     }
