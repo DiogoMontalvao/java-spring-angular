3da06325caf0e9d39a822c2ec58d89379cbf3ab3
Diogo Montalvão <109600744+DiogoMontalvao@users.noreply.github.com>
2025-09-09 09:17:17 -0300

    2.2.2.8 Spring Data JPA

---
+++ spring-data-jpa/src/main/java/com/example/spring_data_jpa/StartApp.java
@@ -0,0 +1,31 @@
+package com.example.spring_data_jpa;
+
+import com.example.spring_data_jpa.model.User;
+import com.example.spring_data_jpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class StartApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private User user;
+
+    @Override
+    public void run(String... args) throws Exception {
+        user.setName("Diogo");
+        user.setUsername("Chatudo");
+        user.setPassword("12345");
+
+        repository.save(user);
+
+        List<User> lista = repository.findAll();
+        System.out.println(lista);
+    }
+}
